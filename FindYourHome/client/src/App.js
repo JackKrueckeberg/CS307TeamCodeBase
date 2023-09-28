3429574628423174779d6fec156393d3f6508a03
Jack Krueckeberg <jackkrueckeberg@gmail.com>
2023-09-28 14:40:08 -0400

    Corrected App.JS file from previous changes

--- FindYourHome/client/src/App.js
+++ FindYourHome/client/src/App.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
- 
+import React from "react";
+
 // We use Route in order to define the different routes of our application
 import { Route, Routes } from "react-router-dom";
  
@@ -8,7 +8,7 @@ import Navbar from "./components/navbar";
 import RecordList from "./components/recordList";
 import Edit from "./components/edit";
 import Create from "./components/create";
-import { Login } from "./components/login";
+import Login from "./components/login";
  
 const App = () => {
   
@@ -19,7 +19,7 @@ const App = () => {
        <Route exact path="/" element={<RecordList />} />
        <Route path="/edit/:id" element={<Edit />} />
        <Route path="/create" element={<Create />} />
-       <Route id="login" path="/login" element={<Login />}/>
+       <Route path="/login" element={<Login />}/>
      </Routes>
       
    </div>
