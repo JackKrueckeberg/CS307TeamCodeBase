da0826bd44f43cbe729761a7e981639e2aa6bee8
nickadair8 <adair8@purdue.edu>
2023-10-01 20:16:35 -0400

    able to query for specific city from search bar

--- FindYourHome/client/src/ViewCity.js
+++ FindYourHome/client/src/ViewCity.js
@@ -2,79 +2,79 @@ import React, { useState, useEffect } from "react";
 import "./ViewCity.css";
 
 const ViewCity = () => {
-  // const [City, setCity] = useState([]);
-  // const [searchTerm, setSearchTerm] = useState("");
-  // const [showResults, setShowResults] = useState(false);
-  // const [clear, setClear] = useState(false);
-
-  const [city, setCity] = useState([]);
+  const [allCities, setAllCities] = useState([]);
+  const [city, setCity] = useState(null);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [showResults, setShowResults] = useState(false);
 
   useEffect(() => {
-    //fetch data from fake data depending on the search
     async function fetchData() {
-      const response = await fetch(`http://localhost:5050/record/cities_full_2`);
-      if (!response.ok) {
-        const message = `An error occurred: ${response.statusText}`;
-        window.alert(message);
-        return;
+      try {
+        const response = await fetch(`http://localhost:5050/record/cities_full_2`);
+        if (!response.ok) {
+          const message = `An error occurred: ${response.statusText}`;
+          window.alert(message);
+          return;
+        }
+
+        const cities = await response.json();
+        setAllCities(cities);
+      } catch (error) {
+        console.error("There was an error fetching the cities", error);
       }
-  
-      const city = await response.json();
-      setCity(city);
-    }; 
+    }
 
     fetchData();
-;
+  }, []);
 
-    return
-  }, [city.length]);
+  const handleSubmit = () => {
+    const matchedCity = allCities.find(c => c.name.toLowerCase() === searchTerm.toLowerCase());
+    setCity(matchedCity);
+    setShowResults(!!matchedCity);
+  };
 
+  const handleClear = () => {
+    setCity(null);
+    setShowResults(false);
+    setSearchTerm("");
+  };
 
-//   //clear the page of divs
-//   const handleClear = () => {
-//     setClear(true);
-//     setShowResults(false);
-//   };
+  const renderResults = () => {
+    if (showResults && city) {
+      return (
+        <div className="result">
+          <h2>{city.name}</h2>
+          <p>{city.country}</p>
+        </div>
+      );
+    } else if (showResults && !city) {
+      return <div className="result"><h2>Invalid Search</h2></div>;
+    }
+    return null;
+  }
 
-//   //display the correct
-//   const renderResults = () => {
-//     if (showResults && City) {
-//       return (<div className="result">
-//         <h2>{City.name}</h2>
-//         <p>{City.country}</p>
-//       </div>);
-//     } else if (clear) {
-//       return (
-//         <div>
-//           <h2></h2>
-//           <p></p>
-//         </div>
-//       );
-//     }
-//     else {
-//       return (<div className="result">
-//         <h2>Invalid Search</h2>
-//       </div>);
-//     }
-//   }
+  return (
+    <div>
+      <h1 className="header">View City Page</h1>
 
-//   return (
-//     <div>
-//       <h1 className="header">View City Page</h1>
+      <div className="searchBar">
+        <label>Search</label>
+        <input
+          type="text"
+          placeholder="Enter a city"
+          value={searchTerm}
+          onChange={e => setSearchTerm(e.target.value)}
+        />
+        <button className="submitButton" onClick={handleSubmit}>Submit</button>
+        <button className="clearButton" onClick={handleClear}>Clear</button>
+      </div>
 
-//       <div className="searchBar">
-//         <label>Search</label>
-//         <input type="text" placeholder="Enter a city" value={searchTerm} onChange={e => { setSearchTerm(e.target.value) }} />
-//         <button className="submitButton" onClick={fetchData}>Submit</button>
-//         <button className="clearButton" onClick={handleClear}>Clear</button>
-//       </div>
+      <div className="renderResults">
+        {renderResults()}
+      </div>
 
-//       <div className="renderResults">
-//         {renderResults()}
-//       </div>
-
-//     </div>
-//   );
+    </div>
+  );
 };
 
 export default ViewCity;
