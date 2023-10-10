77a77a59da0abf708f8a36330e8439653f1a1767
jgoldberg8 <89888020+jgoldberg8@users.noreply.github.com>
2023-10-09 20:09:10 -0700

    hyperlinks for recent cities working

--- FindYourHome/client/src/ViewCity.js
+++ FindYourHome/client/src/ViewCity.js
@@ -1,7 +1,6 @@
-import React, { useState, useEffect } from "react";
-import Autosuggest from 'react-autosuggest'; // Import Autosuggest
+import React, { useState, useEffect, useRef } from "react";
+import Autosuggest from "react-autosuggest";
 import "./ViewCity.css";
-
 
 const ViewCity = () => {
   const [allCities, setAllCities] = useState([]);
@@ -11,11 +10,10 @@ const ViewCity = () => {
   const [cityIncome, setCityIncome] = useState(null);
   const [suggestions, setSuggestions] = useState([]);
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-
-
-
   const [recentSearches, setRecentSearches] = useState([]);
-  const [mockUser, setMockUSer] = useState({
+  const searchFormRef = useRef(null);
+
+  const [mockUser, setMockUser] = useState({
     username: "example_user",
     password: "example_password",
     email: "user@example.com",
@@ -43,38 +41,13 @@ const ViewCity = () => {
 
     fetchCity();
 
-
-    async function fetchUser() {
-      try {
-        const response = await fetch(`http://localhost:5050/record/users`);
-        if (!response.ok) {
-          const message = `An error occurred: ${response.statusText}`;
-          window.alert(message);
-          return;
-        }
-
-        const users = await response.json();
-        console.log(users)
-      } catch (error) {
-        console.error("There was an error fetching the cities", error);
-      }
-    }
-
-    fetchUser();
-
-
     setRecentSearches(mockUser.recent_searches);
   }, [mockUser]);
 
-
-
-
   const updateRecentSearches = (searchTerm) => {
-    // Update the recent_searches field of the mock user
     mockUser.recent_searches.push(searchTerm);
     setRecentSearches([...mockUser.recent_searches]);
   };
-
 
   const onSuggestionsFetchRequested = ({ value }) => {
     if (value) {
@@ -93,13 +66,21 @@ const ViewCity = () => {
     setSuggestions([]);
   };
 
-  const handleSubmit = () => {
-    const matchedCity = allCities.find((c) => c.name.toLowerCase() === searchTerm.toLowerCase());
+  const handleCityClick = (suggestion) => {
+    if (suggestion && suggestion.trim()) {
+      setSearchTerm(suggestion);
+      handleSubmit(suggestion);
+    }
+  };
+
+  const handleSubmit = (searchTerm) => {
+    const matchedCity = allCities.find(
+      (c) => c.name.toLowerCase() === searchTerm.toLowerCase()
+    );
     setCity(matchedCity);
     setCityIncome(matchedCity ? matchedCity.median_income : null);
     setShowResults(true);
     updateRecentSearches(searchTerm);
-
   };
 
   const handleClear = () => {
@@ -113,16 +94,7 @@ const ViewCity = () => {
     setIsDropdownOpen(true);
   };
 
-  const handleSuggestionClick = (suggestion) => {
-    setSearchTerm(suggestion);
-    setIsDropdownOpen(false);
-  };
-
   const renderResults = () => {
-    
-    
-    
-    
     if (showResults && city) {
       return (
         <div className="result">
@@ -136,10 +108,8 @@ const ViewCity = () => {
       }
       return <div className="result"><h2>Invalid Search</h2></div>;
     }
-    
     return null;
   };
-
 
   const renderRecentSearches = () => {
     if (recentSearches.length === 0) {
@@ -151,20 +121,26 @@ const ViewCity = () => {
         <h3>Recent Searches:</h3>
         <ul>
           {recentSearches.map((search, index) => (
-            <li key={index}>{search}</li>
+            <li
+              key={index}
+              onClick={() => handleCityClick(search)}
+              style={{ cursor: "pointer" }}
+            >
+              {search}
+            </li>
           ))}
         </ul>
       </div>
     );
   };
-  
+
   return (
     <div>
       <h1 className="header">View City Page</h1>
 
-      <div className="searchBar">
+      <form ref={searchFormRef} onSubmit={(e) => e.preventDefault()} className="searchBar">
         <label>Search</label>
-        <Autosuggest // Use Autosuggest component
+        <Autosuggest
           suggestions={suggestions}
           onSuggestionsFetchRequested={onSuggestionsFetchRequested}
           onSuggestionsClearRequested={onSuggestionsClearRequested}
@@ -173,7 +149,7 @@ const ViewCity = () => {
             <div
               key={suggestion}
               className="suggestion"
-              onClick={() => handleSuggestionClick(suggestion)}
+              onClick={() => handleCityClick(suggestion)}
             >
               {suggestion}
             </div>
@@ -185,34 +161,14 @@ const ViewCity = () => {
             onChange: handleInputChange,
           }}
         />
-        <button className="submitButton" onClick={handleSubmit}>Submit</button>
+        <button className="submitButton" onClick={() => handleSubmit(searchTerm)}>Submit</button>
         <button className="clearButton" onClick={handleClear}>Clear</button>
-      </div>
+      </form>
 
-      <div className="renderResults">
-        {renderResults()}
-      </div>
+      <div className="renderResults">{renderResults()}</div>
       {renderRecentSearches()}
-
     </div>
   );
 };
 
 export default ViewCity;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
