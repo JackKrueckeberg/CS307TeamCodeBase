83fe80052b00f13dfb6b87cbe3c3018b13e3abb7
Jason Bodzy <jasonbodzy@gmail.com>
2023-10-24 21:32:26 -0400

    added navigation

--- FindYourHome/client/src/components/citypage.js
+++ FindYourHome/client/src/components/citypage.js
@@ -8,14 +8,20 @@ import Twitter from "./twitter";
 import { useCity } from "../contexts/CityContext";
 
 
+
 export default function CityPage(props) {
     const currentUser = localStorage.getItem("currentCity");
     const {city: globalCity} = useCity();
+    const navigate = useNavigate();
 
 
 
   return (
     <div>
+        <div className="nav-buttons">
+            <button className="viewCity-button" onClick={() => navigate('/view-city')}>Go to City Search</button>
+            <button className="preferences-button" onClick={() => navigate('/preferences')}>Go to Preferences</button>
+        </div>
         <div className="result">
             <label content={globalCity.lon}></label>
             {globalCity.name && globalCity && <CityModel model={globalCity} />}
