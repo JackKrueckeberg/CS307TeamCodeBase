64c2b8726143d329267f4da2b008d7d9bad80bc1
nickadair8 <adair8@purdue.edu>
2023-10-31 10:08:06 -0400

    compare cities page is done

--- FindYourHome/client/src/components/compareCities.js
+++ FindYourHome/client/src/components/compareCities.js
@@ -1,19 +1,24 @@
 import React from "react";
-import { CityModel, Model } from "./CityModel/CityModel";
+import { CityModel } from "./CityModel/CityModel";
 import { useNavigate } from "react-router-dom";
 
 export default function CompareCities() {
     // Retrieve cities from localStorage
     const citiesToCompare = JSON.parse(localStorage.getItem('compareCities') || '[]');
-    console.log(citiesToCompare);
     const navigate = useNavigate();
 
     function handleBackButton() {
         localStorage.removeItem('compareCities');
         navigate("/view-city");
     }
+    
     return (
         <div>
+            {/* Display city details */}
+            {citiesToCompare.map((city, index) => (
+                <CityModel key={index} model={city} />
+            ))}
+
             <div>
                 <button onClick={handleBackButton}>Back to View City</button>
             </div>
