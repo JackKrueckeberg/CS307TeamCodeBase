081537892eea04cb3585b4aa6d5e1a5ff73712c0
nickadair8 <adair8@purdue.edu>
2023-11-29 21:25:09 -0500

    suggested cities is basically finished, need to clean up the like button and the clear button

--- FindYourHome/client/src/components/profile.js
+++ FindYourHome/client/src/components/profile.js
@@ -9,10 +9,14 @@ import MessageNotification from "./messageNotification";
 import Favorites from "./favorites.js";
 import PageAnimation from "../animations/PageAnimation.jsx";
 import { useUser } from "../contexts/UserContext";
+const OPENAI_API_KEY = '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'
 
 export default function Profile() {
   const storedSesUser = JSON.parse(sessionStorage.getItem("currentUser"));
   const storedLocUser = JSON.parse(localStorage.getItem("currentUser"));
+  const [favoriteCities, setFavoriteCities] = useState([]);
+  const [suggestedCities, setSuggestedCities] = useState([]);
+  const [likedCities, setLikedCities] = useState(new Set());
 
   const { user: userProfile } = useUser(); // the id of the current logged in user
   const [user, setInfo] = useState(
@@ -28,7 +32,7 @@ export default function Profile() {
   const navigate = useNavigate();
   const [topCities, setTopCities] = useState([]);
 
-  
+
   // Define the getTopThreeCities function
   const getTopThreeCities = (usageStats) => {
     const citiesArray = Object.entries(usageStats);
@@ -67,6 +71,89 @@ export default function Profile() {
     sessionStorage.removeItem("currentUser");
 
     navigate("/", { state: { loggedOut: true }, replace: true });
+  };
+
+
+  // Fetch Favorite Cities
+  useEffect(() => {
+    const fetchFavoriteCities = async () => {
+      try {
+        const response = await fetch(`http://localhost:5050/favorite_cities/${g_email}`);
+        if (!response.ok) {
+          throw new Error('Failed to fetch favorite cities');
+        }
+        const data = await response.json();
+        setFavoriteCities(data);
+      } catch (error) {
+        console.error("Error fetching favorite cities:", error);
+      }
+    };
+
+    fetchFavoriteCities();
+  }, [g_email]);
+
+  // Fetch Suggested Cities
+  useEffect(() => {
+    favoriteCities.forEach(city => {
+      fetchSuggestedCities(city);
+    });
+  }, [favoriteCities]);
+
+  async function fetchSuggestedCities(city) {
+    try {
+      const response = await fetch('https://api.openai.com/v1/chat/completions', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${OPENAI_API_KEY}`,
+        },
+        body: JSON.stringify({
+          model: 'gpt-3.5-turbo',
+          messages: [{ role: 'user', content: `What is one city similar to ${city}? Only give the name of the city.` }],
+          temperature: 0.7,
+          top_p: 1,
+          n: 1,
+          stream: false,
+          presence_penalty: 0,
+          frequency_penalty: 0,
+        }),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        updateSuggestedCities(data.choices[0].message.content.trim());
+      } else {
+        console.log('Error: Unable to process your request.');
+      }
+    } catch (error) {
+      console.error(error);
+      console.log('Error: Unable to process your request.');
+    }
+  }
+
+  function updateSuggestedCities(suggestedCity) {
+    if (suggestedCity && !suggestedCities.includes(suggestedCity)) {
+      setSuggestedCities(prevList => [...prevList, suggestedCity]);
+    }
+  }
+
+  const handleLike = (cityToLike) => {
+    setLikedCities(prevLikedCities => new Set(prevLikedCities.add(cityToLike)));
+  };
+
+  // Function to handle disliking a city
+  const handleDislike = (cityToDislike) => {
+    setSuggestedCities(prevList => prevList.filter(city => city !== cityToDislike));
+    setLikedCities(prevLikedCities => {
+      const newLikedCities = new Set(prevLikedCities);
+      newLikedCities.delete(cityToDislike);
+      return newLikedCities;
+    });
+  };
+
+  // Clear Suggested Cities
+  const clearSuggestedCities = () => {
+    setSuggestedCities([]);
   };
 
   return (
@@ -157,13 +244,35 @@ export default function Profile() {
           </div>
 
           <div className="top-cities">
-        <h3>Top Searched Cities</h3>
-        <ul>
-          {topCities.map((city, index) => (
-            <li key={index}>{`${city[0]}: ${city[1]} searches`}</li>
-          ))}
-        </ul>
-      </div>
+            <h3>Top Searched Cities</h3>
+            <ul>
+              {topCities.map((city, index) => (
+                <li key={index}>{`${city[1]} searches`}</li>
+              ))}
+            </ul>
+          </div>
+
+          <div>
+            <h2>Suggested Cities</h2>
+            {suggestedCities.length > 0 ? (
+              <ul>
+                {suggestedCities.map((city, index) => (
+                  <li key={index}>
+                    {city}
+                    {!likedCities.has(city) && (
+                      <>
+                        <button onClick={() => handleLike(city)}>Like</button>
+                        <button onClick={() => handleDislike(city)}>Dislike</button>
+                      </>
+                    )}
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <p>No suggested cities available.</p>
+            )}
+          </div>
+
 
           <div
             className={`${tabVal === 2 ? "content active-content" : "content"}`}
